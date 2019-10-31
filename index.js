f617dd99e52ca5d44f6f127232850b677073cced
radville <laura.radville@gmail.com>
2019-10-31 09:25:10 -0400

    Done.

--- index.js
+++ index.js
@@ -6,17 +6,28 @@ const fi = (function() {
   
     each: function(collection, callback) {
       let i;
-      for (i=0; i < collection.length; i++) {
-        callback(collection[i]);
+      let newCollection;
+      if (collection instanceof Array) {
+        newCollection = collection.slice()
+      } else { 
+        newCollection = Object.values(collection)
       }
+      for (i=0; i < newCollection.length; i++) {
+        callback(newCollection[i])      }
       return collection
     },
 
     map: function(collection, callback) {
       let i;
       let newArray = []
-      for (i=0; i < collection.length; i++) {
-        newArray.push(callback(collection[i]));
+      let newCollection;
+      if (collection instanceof Array) {
+        newCollection = collection.slice()
+      } else { 
+        newCollection = Object.values(collection)
+      }
+      for (i=0; i < newCollection.length; i++) {
+        newArray.push(callback(newCollection[i]));
       }
       return newArray
     },
@@ -24,8 +35,12 @@ const fi = (function() {
     reduce: function(collection, callback, startingValue) {
       let total = startingValue;
       let i;
+      if (!startingValue) {
+        total = collection[0];
+        collection = collection.slice(1)
+      }
       for (i=0; i < collection.length; i++) {
-        total = callback(collection[i], total)
+        total = callback(total, collection[i])
       }
       return total
     },
@@ -34,7 +49,7 @@ const fi = (function() {
       const isFunction = value => value && (Object.prototype.toString.call(value) === "[object Function]" || "function" === typeof value || value instanceof Function);
       let i;
       let newArray = [];
-      console.log(object)
+      // console.log(object)
       // for (i=0, i < object.values)
       // object.values.forEach(value => {
       //   if (isFunction(value)) {
@@ -56,16 +71,30 @@ const fi = (function() {
 
     size: function(collection) {
       let i;
-      for (i=0; i < collection.length; i++) {}
-      return i
+      let size = 0;
+      if (collection.length > 0) {
+        for (i=0; i < collection.length; i++) {
+          ++size;
+      }} else {
+        size = Object.keys(collection).length
+      }
+      return size
     },
 
-    first: function(array, number) {
-
+    first: function(array, number=1) {
+      if (number === 1) {
+        return array.slice(0,(number))[0]
+      } else {
+        return array.slice(0,(number))
+      }
     },
 
-    last: function(array, number) {
-
+    last: function(array, number=1) {
+      if (number === 1) {
+        return array.slice(number * -1)[0]
+      } else {
+        return array.slice(number * -1)
+      }
     },
     
     compact: function(array) {
@@ -73,7 +102,9 @@ const fi = (function() {
     },
 
     sortBy: function(array, callback) {
-
+      let newArray = array.slice();
+      newArray.sort(function(a, b){return callback(a)-callback(b)});
+      return newArray;
     },
 
     flatten: function(array, shallow) {
